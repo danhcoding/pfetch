fef616ac9cfb0c23307005cead271e1fe45b7fd5
danhcoding <153963148+danhcoding@users.noreply.github.com>
2025-03-13 17:46:31 +0000

    update to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build', // Custom build directory
+    basePath: '/pfetch',
+    assetPrefix: '/pfetch/',
 };
 
 export default nextConfig;
